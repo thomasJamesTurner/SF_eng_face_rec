3bd3ca1058f06c71dad1800e098dcf7f32f24ab8
qro <vershbow_polarizer@8shield.net>
2024-11-28 18:27:29 +0000

    + sql configuration

--- Administration/test.py
+++ Administration/test.py
@@ -1,2 +1,13 @@
 import os, flask, SQLAlchemy, Bcrypt, MySQL
 
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+
+app.secret_key = ''
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'sqluser'
+app.config['MYSQL_PASSWORD'] = 'sqlpassword'
+app.config['MYSQL_DB'] = 'face_recognition'
+
+mysql = MySQL(app)
+
