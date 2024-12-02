2ec526aa51a469d1e7994fda5d73e9cab5460f1a
qro <vershbow_polarizer@8shield.net>
2024-12-02 16:29:46 +0000

    + login and dashboard sessions

--- Administration/test.py
+++ Administration/test.py
@@ -11,3 +11,33 @@ app.config['MYSQL_DB'] = 'face_recognition'
 
 mysql = MySQL(app)
 
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    cur = mysql.connection.cursor()
+    teacher = cur.fetchone()
+    cur.close()
+
+    if teacher and bcrypt.check_password_hash(teacher[2], password):
+        sessions['teacher_id'] = teacher[0]
+        return redirect(url_for('dashboard'))
+    else:
+        return "Invalid username or password"
+
+@app.route('/dashboard')
+def dashboard():
+    if 'teacher_id' not in session:
+        return redirect(url_for('home'))
+    
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM students")
+    students = cur.fetchall()
+    cur.close()
+
+    return render_template('dashboard.html', students=students)
